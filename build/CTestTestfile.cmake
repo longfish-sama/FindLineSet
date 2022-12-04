a585f159bd8a46580f7d25b6cfa2f1673e74b779
longfish-sama <77600597+longfish-sama@users.noreply.github.com>
2022-12-05 00:11:11 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/chend/Documents/code/xlnt-1.5.0
-# Build directory: D:/chend/Documents/code/2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
-subdirs("source")
--- build/source/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/chend/Documents/code/xlnt-1.5.0/source
-# Build directory: D:/chend/Documents/code/2/build/source
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("third-party/libstudxml")
